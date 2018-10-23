eca2c62f8034ceb30fdcc572a1f25f5fff7c615d
bsasaki <43330093+bsasaki@users.noreply.github.com>
2018-10-23 11:51:23 -0700

    Add files via upload

--- Header.h
+++ Header.h
@@ -4,7 +4,6 @@
 #include <fstream>
 #include <vector>
 #include <string>
-#include <mutex>
 #include <omp.h>
 
 struct myMap {
@@ -12,12 +11,11 @@ struct myMap {
 	int value;
 };
 
-void map_thread(std::vector<std::string>, int tid);
-void reduce_thread(std::vector<std::vector<myMap>> maps, int tid);
-std::vector<myMap> first_word_counter(std::ifstream& in_file);
+void map_thread(std::string key);
+void reduce_thread(std::vector<myMap> keys);
 std::vector<std::string> input_read(std::ifstream& in_file);
 myMap create_pair(std::string key);
-myMap reduce(std::vector<myMap> v1);
+myMap reduce(std::vector<myMap> v);
 void output(myMap m);
 void bubblesort(std::vector<myMap> &strings);
 void sort_by_value(std::vector<myMap> &strings);
--- main.cpp
+++ main.cpp
@@ -7,102 +7,77 @@ Authors: Bennett Sasaki & Jordan Kam
 using namespace std;
 
 constexpr int MAX_THREADS = 4;
-mutex mtx;
 ofstream out_file;
 std::vector<myMap> maps;
 std::vector<myMap> reduced_pairs;
-std::chrono::time_point<std::chrono::high_resolution_clock, std::chrono::high_resolution_clock::duration> init_time;
 double single_time, map_time = 0;
 
 
 int main() {
 	ifstream in_file;
-	out_file.open("mapReduceOut_____.txt");
+	out_file.open("mapReduceOut.txt");
 	in_file.open("L4In.txt", std::ifstream::in);
-	out_file << "\n---------------MapReduce---------------\n";
+	out_file << "---------------MapReduce---------------\n";
 	std::vector<std::string> words; //Vector to hold unsorted words
 	words = input_read(in_file);
 
 #pragma omp parallel for
 	for (int i = 0; i < words.size(); i++)
-	{
-		map_thread(words, i);
-	}
+		map_thread(words[i]);
 
 	bubblesort(maps); //maps is now sorted alphabetically
 	std::vector<std::vector<myMap>> groups_of_keys = pre_reduce(maps); //groups_of_keys now holds vectors that each hold pairs with the same keys
 
 #pragma omp parallel for
-	for (int i = 0; i < groups_of_keys.size(); i++) {
-		reduce_thread(groups_of_keys, i);
-	}
+	for (int i = 0; i < groups_of_keys.size(); i++)
+		reduce_thread(groups_of_keys[i]);
 
 	sort_by_value(reduced_pairs);
 
-	for (int i = 0; i < reduced_pairs.size(); i++) {
+	for (int i = 0; i < reduced_pairs.size(); i++)
 		output(reduced_pairs[i]);
-	}
 
 	in_file.close();
+	out_file.close();
 	return 0;
 }
 
-void map_thread(std::vector<std::string> words, int tid) {
+void map_thread(std::string key) {
+/* map_thread- section 4.2
+   Purpose: creates key-value pair
+   Input: a single key
+   Output: key-value pair with a value of 1
+*/
 	myMap m;
-	mtx.lock();
-	m = create_pair(words[tid]);
+#pragma omp critical
+	{
+	m = create_pair(key);
 	maps.push_back(m);
-	mtx.unlock();
-}
-
-void reduce_thread(std::vector<std::vector<myMap>> maps, int tid) {
-	myMap m;
-	mtx.lock();
-	m = reduce(maps[tid]);
-	reduced_pairs.push_back(m);
-	mtx.unlock();
-}
-
-std::vector<myMap> first_word_counter(ifstream& in_file) {
-	/* word_counter- Section 3
-	   Purpose: return word count
-	   Input: text file to perform word count on
-	   Output: total word count
-	   Note: does not check for delimeters, only spaces.
-			 We can fix that later if we need to
-	*/
-	std::vector<myMap> pairs;
-	myMap m;
-	char* word = new char;
-	while (in_file >> word) {
-		int i = 0;
-		while (true) {
-			if (i == pairs.size()) {
-				m.key = word;
-				m.value = 1;
-				pairs.push_back(m);
-				break;
-			}
-			else if (word == pairs[i].key) {
-				pairs[i].value++;
-				break;
-			}
-			i++;
-		}
-
 	}
+}
 
-	return pairs;
+void reduce_thread(std::vector<myMap> pairs) {
+/* reduce_thread- section 4.2
+	Purpose: creates a single key-value pair and pushes into reduced_pairs
+	Input: vector with pairs, all with the same keys
+	Output: N/A
+*/
+	myMap m;
+#pragma omp critical 
+	{
+	m = reduce(pairs);
+	reduced_pairs.push_back(m);
+	}
 }
 
 std::vector<std::string> input_read(std::ifstream& in_file) {
-	/* input_read- Section 4.1.1
-	   Purpose: return vector where each memeber is a word
-	   Input: text file to perform word count on
-	   Output: vector with each word separated
-	   Note: does not check for delimeters, only spaces.
-			 We can fix that later if we need to
-	*/
+/*  input_read- Section 4.1.1
+	Purpose: return vector where each memeber is a word
+	Input: text file to perform word count on
+	Output: vector with each word separated
+	Note: does not check for delimeters, only spaces.
+			We can fix that later if we need to
+*/
 	std::vector<std::string> vect;
 	char* word = new char;
 	while (in_file >> word) {
@@ -112,37 +87,35 @@ std::vector<std::string> input_read(std::ifstream& in_file) {
 }
 
 myMap create_pair(std::string key) {
-	/* create_pair- Section 4.1.2
-	   Formerly called "map_func"
-	   Purpose: creates a key-value pair with a value one for a given key
-	   Input: string which will be the key of the key-value pair
-	   Output: key-value pair
-	*/
+/*  create_pair- Section 4.1.2
+	Formerly called "map_func"
+	Purpose: creates a key-value pair with a value one for a given key
+	Input: string which will be the key of the key-value pair
+	Output: key-value pair
+*/
 	myMap m;
 	m.key = key;
 	m.value = 1;
-	//out_file << m.key << " " << m.value << endl;
 	return m;
 }
 
-myMap reduce(std::vector<myMap> v1) {
-	/* create_pair- Section 4.1.3
-	   Purpose: creates a single key-value pair given a list of pairs with the same keys
-	   Input: list of key-value pairs, which should have the same key
-	   Output: a single key-value pair with correct value
-	*/
-
-	myMap m1;
-	m1.key = v1[0].key;
-	m1.value = v1.size();
-	return m1;
+myMap reduce(std::vector<myMap> v) {
+/*  reduce- Section 4.1.3
+	Purpose: creates a single key-value pair given a list of pairs with the same keys
+	Input: list of key-value pairs, which should have the same key
+	Output: a single key-value pair with correct value
+*/
+	myMap m;
+	m.key = v[0].key;
+	m.value = v.size();
+	return m;
 }
 
 void output(myMap m) {
-	/* create_pair- Section 4.1.4
-   Purpose: outputs word count for a given key-value pair
-   Input: a single key-value pair
-   Output: N/A
+/*  create_pair- Section 4.1.4
+	Purpose: outputs word count for a given key-value pair
+	Input: a single key-value pair
+	Output: N/A
 */
 	out_file << m.key << ": " << m.value << "\n";
 	return;
@@ -150,10 +123,8 @@ void output(myMap m) {
 
 void bubblesort(std::vector<myMap> &strings) {
 	typedef std::vector<myMap>::size_type size_type;
-	for (size_type i = 1; i < strings.size(); ++i) // for n-1 passes
+	for (size_type i = 1; i < strings.size(); ++i)
 	{
-		// In pass i,compare the first n-i elements
-		// with their next elements
 		for (size_type j = 0; j < (strings.size() - 1); ++j)
 		{
 			if (strings[j].key > strings[j + 1].key)
@@ -168,10 +139,8 @@ void bubblesort(std::vector<myMap> &strings) {
 
 void sort_by_value(std::vector<myMap> &strings) {
 	typedef std::vector<myMap>::size_type size_type;
-	for (size_type i = 1; i < strings.size(); ++i) // for n-1 passes
+	for (size_type i = 1; i < strings.size(); ++i)
 	{
-		// In pass i,compare the first n-i elements
-		// with their next elements
 		for (size_type j = 0; j < (strings.size() - 1); ++j)
 		{
 			if (strings[j].value < strings[j + 1].value)
@@ -185,11 +154,11 @@ void sort_by_value(std::vector<myMap> &strings) {
 }
 
 std::vector<std::vector<myMap>> pre_reduce(std::vector<myMap> single_words) {
-	/* pre-reduce
-	   this function takes the bubble-sorted pairs and groups together the pairs with the same keys, so they can go into reduce
+/*  pre-reduce
+	this function takes the bubble-sorted pairs and groups together the pairs with the same keys, so they can go into reduce
 	Input: Vector of maps, already sorted alphabetically
 	Output: Vector of vectors of maps, each vector holding all the pairs with the same key
-	*/
+*/
 	std::vector<std::vector<myMap>> master;
 	std::vector<myMap> dummy;
 	dummy.push_back(single_words[0]);
